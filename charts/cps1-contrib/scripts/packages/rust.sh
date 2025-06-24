f3deb6e4d67a64761f57c5319041b1d331ad32f1
Miguel Di Ciurcio Filho <miguel@cps1.tech>
2025-06-24 20:46:26 -0300

    feat(contrib): implement Rust package

--- charts/cps1-contrib/scripts/packages/rust.sh
+++ charts/cps1-contrib/scripts/packages/rust.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-return 0
+HOME=/home/user
+
+apt-get update
+
+apt-get install -y gcc
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs --output /tmp/rustup.sh
+
+su user -c "bash /tmp/rustup.sh -y"
+
+rm /tmp/rustup.sh
+
+# shellcheck disable=SC1091
+. "${HOME}/.cargo/env"
+
+rustc --version
