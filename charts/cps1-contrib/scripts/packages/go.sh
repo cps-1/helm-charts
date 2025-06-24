22b057ef5c4a95c89535e82f73accb6cba2bf7d9
Miguel Di Ciurcio Filho <miguel@cps1.tech>
2025-06-24 20:27:06 -0300

    feat(contrib): implement Go package

--- charts/cps1-contrib/scripts/packages/go.sh
+++ charts/cps1-contrib/scripts/packages/go.sh
@@ -2,4 +2,24 @@
 
 set -euo pipefail
 
-return 0
+HOME=/home/user
+
+# shellcheck disable=SC2223
+: ${CPS1_GO_VERSION:="1.24.4"}
+
+cd $HOME
+curl -L "https://go.dev/dl/go${CPS1_GO_VERSION}.linux-amd64.tar.gz" --output "${HOME}/go.tar.gz"
+
+tar xzf go.tar.gz
+mv go .golang
+rm go.tar.gz
+
+# shellcheck disable=SC2016
+echo 'export "PATH=${HOME}/.golang/bin:${PATH}"' >> ${HOME}/.bashrc
+
+export "PATH=${HOME}/.golang/bin:${PATH}"
+
+chown -R user:user ${HOME}/.golang/
+
+go version
+
