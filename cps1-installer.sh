01d0f695ee06269c96d7e3331493838de80674cb
Miguel Di Ciurcio Filho <miguel@cps1.tech>
2025-06-24 21:49:26 -0300

    fix: make installer safer for curl | bash

--- cps1-installer.sh
+++ cps1-installer.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
-set -eu
 
-commands=("kind" "docker" "helm" "kubectl")
-missing=()
+set -euo pipefail
 
-echo "Checking requirements..."
+# This script installs CPS1 on a local environment for evaluation.
+# For a production grade installation, refer to our documentation https://docs.cps1.tech
 
-for cmd in "${commands[@]}"; do
-  if which "$cmd" > /dev/null 2>&1; then
-    echo "✅ '$cmd' is available."
-  else
-    echo "❌ '$cmd' is MISSING."
-    missing+=("$cmd")
-  fi
-done
+check_dependencies () {
+  commands=("kind" "docker" "helm" "kubectl")
+  missing=()
 
-if [ ${#missing[@]} -ne 0 ]; then
-  echo ""
-  echo "The following commands were not found:"
-  for m in "${missing[@]}"; do
-    echo " - $m"
+  echo "Checking requirements..."
+
+  for cmd in "${commands[@]}"; do
+    if which "$cmd" > /dev/null 2>&1; then
+      echo "✅ '$cmd' is available."
+    else
+      echo "❌ '$cmd' is MISSING."
+      missing+=("$cmd")
+    fi
   done
-  exit 1
-fi
 
-kind delete cluster -n cps1
+  if [ ${#missing[@]} -ne 0 ]; then
+    echo ""
+    echo "The following commands were not found:"
+    for m in "${missing[@]}"; do
+      echo " - $m"
+    done
+    exit 1
+  fi
+}
 
-cat <<EOF | kind create cluster -n cps1 --config=-
+do_install () {
+
+  check_dependencies
+
+  kind delete cluster -n cps1
+
+  cat <<EOF | kind create cluster -n cps1 --config=-
 apiVersion: kind.x-k8s.io/v1alpha4
 kind: Cluster
 nodes:
@@ -43,29 +53,32 @@ nodes:
     protocol: TCP
 EOF
 
-kubectl config set-context kind-cps1 --namespace cps1
+  kubectl config set-context kind-cps1 --namespace cps1
 
-BUILDER_IMAGE="quay.io/buildah/stable:latest"
-BASE_IMAGE="ghcr.io/cps-1/base-image:latest"
-CPS1_IMAGE="ghcr.io/cps-1/cps1:v0.1.0-rc7"
+  BUILDER_IMAGE="quay.io/buildah/stable:latest"
+  BASE_IMAGE="ghcr.io/cps-1/base-image:20250624"
+  CPS1_IMAGE="ghcr.io/cps-1/cps1:v0.1.0-rc7"
 
-docker pull "${BUILDER_IMAGE}"
-docker pull "${BASE_IMAGE}"
-docker pull "${CPS1_IMAGE}"
+  docker pull "${BUILDER_IMAGE}"
+  docker pull "${BASE_IMAGE}"
+  docker pull "${CPS1_IMAGE}"
 
-kind load docker-image -n cps1 "${BUILDER_IMAGE}"
-kind load docker-image -n cps1 "${BASE_IMAGE}"
-kind load docker-image -n cps1 "${CPS1_IMAGE}"
+  kind load docker-image -n cps1 "${BUILDER_IMAGE}"
+  kind load docker-image -n cps1 "${BASE_IMAGE}"
+  kind load docker-image -n cps1 "${CPS1_IMAGE}"
 
-kubectl create namespace cps1
+  kubectl create namespace cps1
 
-helm repo add cps1 https://cps-1.github.io/helm-charts/
+  helm repo add cps1 https://cps-1.github.io/helm-charts/
 
-helm repo update
+  helm repo update
 
-helm install -n cps1 --devel cps1-crds cps1/cps1-crds
+  helm install -n cps1 --devel cps1-crds cps1/cps1-crds
 
-helm install -n cps1 --devel cps1-platform cps1/cps1-platform
+  helm install -n cps1 --devel cps1-platform cps1/cps1-platform
 
-helm install -n cps1 --devel cps1-contrib cps1/cps1-contrib
+  helm install -n cps1 --devel cps1-contrib cps1/cps1-contrib
 
+}
+
+do_install
