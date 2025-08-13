ff1230fb72ef6b02ef7d10806db69b4334436c91
Miguel Di Ciurcio Filho <miguel@cps1.tech>
2025-08-13 14:32:43 -0300

    contrib(fix): use arrays on APT package

--- charts/cps1-contrib/scripts/packages/apt.sh
+++ charts/cps1-contrib/scripts/packages/apt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-PACKAGES=$(echo "$CPS1_APT_PACKAGES" | tr -d '[]' | tr ',' ' ')
+IFS=',' read -r -a PACKAGES <<< "$(echo "$CPS1_APT_PACKAGES" | tr -d '[:space:][]')"
+
 apt-get update
-apt-get install -y "$PACKAGES"
+apt-get install -y "${PACKAGES[@]}"
